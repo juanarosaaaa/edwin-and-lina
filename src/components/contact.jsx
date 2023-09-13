cc84f2b899c5b29c3b12aaea8cda764fddbe8219
Jann Rose Tiongco <jhannprose@gmail.com>
2023-09-14 00:21:37 +0800

    edit footer

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -149,10 +149,15 @@ export const Contact = (props) => {
       <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
+            &copy; 2023 Edwin and Lina Poultry Farm | {" "}
+            <a href="" rel="">
+              Privacy 
             </a>
+            | {" "}
+            <a href="">
+              Site Terms 
+            </a>
+            | All rights reserved.
           </p>
         </div>
       </div>
