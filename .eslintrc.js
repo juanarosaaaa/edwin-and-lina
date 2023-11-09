0996fa7375a5ec12ff4f38d4b95ee794d4613ed9
Jann Rose Tiongco <jhannprose@gmail.com>
2023-11-10 02:07:09 +0800

    add animation for the header

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    plugins: ["jquery"],
+    env: {
+      browser: true,
+      es6: true,
+      jquery: true,
+    },
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+    // ... other configurations
+  };
+  
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,40 @@
-import React from "react";
+/* eslint-env jquery */
+import React, { useEffect } from "react";
 
 export const Header = (props) => {
+  const images = ["intro-bg-poultry.JPG", "image1.jpg", "image2.jpg", "image3.jpg", "image4.JPG", "image5.JPG"];
+
+  const preloadImages = (imageArray) => {
+    for (const imageSrc of imageArray) {
+      const img = new Image();
+      img.src = `../img/${imageSrc}`;
+    }
+  };
+
+  useEffect(() => {
+    // Preload images before starting the slideshow
+    preloadImages(images);
+    
+    // Add your slideshow initialization code here
+    $(".intro").css("background-image", "url(../img/intro-bg-poultry.JPG)");
+    startSlideshow();
+  }, []);
+
+  const startSlideshow = () => {
+    let currentIndex = 0;
+
+    const slideToLeft = () => {
+      currentIndex = (currentIndex + 1) % images.length;
+      const imageUrl = `url(../img/${images[currentIndex]})`;
+  
+      $(".intro").css("background-image", imageUrl);
+      $(".intro").css("background-position", "100% 50%");
+      $(".intro").animate({ backgroundPosition: "0% 50%" }, 2000, "linear", slideToLeft);
+    };
+  
+    slideToLeft(); // Start the initial slide
+  };
+
   return (
     <header id="header">
       <div className="intro">
@@ -13,10 +47,7 @@ export const Header = (props) => {
                   <span></span>
                 </h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
-                <a
-                  href="#highlights"
-                  className="btn btn-custom btn-lg page-scroll"
-                >
+                <a href="#highlights" className="btn btn-custom btn-lg page-scroll">
                   Explore Now
                 </a>{" "}
               </div>
